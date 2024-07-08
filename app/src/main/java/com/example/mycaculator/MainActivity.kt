a34b3012a3400ccea9cb8b4aebc5d5ac029ac6f2
thiennh23 <21522626@gm.uit.edu.vn>
2024-07-08 09:39:45 +0700

    Finish logic

--- app/src/main/java/com/example/mycaculator/MainActivity.kt
+++ app/src/main/java/com/example/mycaculator/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.mycaculator
 
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycaculator.ui.theme.MyCaculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +39,82 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyCaculatorTheme {
+            Caculator()
+            /*MyCaculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
-            }
+            }*/
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Caculator() {
+
+    val mContext = LocalContext.current
+
+    var num1 by remember {
+        mutableStateOf("0")
+    }
+
+    var num2 by remember {
+        mutableStateOf("0")
+    }
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(top = 100.dp, start = 15.dp, end = 15.dp)) {
+        Text(text = "CACULATOR", Modifier.fillMaxWidth(), fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+        TextField(value = num1, onValueChange = {num1 = it}, Modifier.fillMaxWidth())
+        TextField(value = num2, onValueChange = {num2 = it}, Modifier.fillMaxWidth())
+
+        Row (Modifier.padding(start = 15.dp, top = 15.dp)) {
+            Button(onClick = {
+                var result: Int = num1.toInt() + num2.toInt()
+                Toast.makeText(mContext,"Result is $result", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(text = "Add")
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(onClick = {
+                var result: Int = num1.toInt() - num2.toInt()
+                Toast.makeText(mContext, "Result is $result", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(text = "Sub")
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(onClick = {
+                var result: Int = num1.toInt() * num2.toInt()
+                Toast.makeText(mContext, "Result is $result", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(text = "Mul")
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+            
+            Button(onClick = {
+                var result: Int = num1.toInt() / num2.toInt()
+                Toast.makeText(mContext, "Result is $result", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(text = "Div")
+            }
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    MyCaculatorTheme {
-        Greeting("Android")
-    }
+fun CaculatorPreview() {
+    Caculator()
 }
